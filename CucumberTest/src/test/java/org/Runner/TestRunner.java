094000b5cf9184fc3b3d66fc50a97b008e2ca1c6
Sate <Sate@DESKTOP-BQ2G7E1>
2020-05-09 01:57:49 +0530

    Maven Commit try

--- CucumberTest/src/test/java/org/Runner/TestRunner.java
+++ CucumberTest/src/test/java/org/Runner/TestRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith (Cucumber.class)
-@CucumberOptions(features="C:\\Users\\Sate\\eclipse-workspace\\CucumberTest\\Feature" ,glue = "org.stepsefinition",monochrome= true,strict= true)
+@CucumberOptions(features="C:\\Users\\Sate\\git\\Testing\\CucumberTest\\Feature\\Login.feature",glue = {"org.stepsefinition"},monochrome= true,dryRun= false)
 public class TestRunner {
 
 }
